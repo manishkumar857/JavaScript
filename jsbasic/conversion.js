319984a95b40c133e36b1644484c2e723d63aa12
Manish Kumar <132132840+manishkumar857@users.noreply.github.com>
2024-07-02 12:04:06 +0000

    conversion of datatypes

---
+++ jsbasic/conversion.js
@@ -0,0 +1,21 @@
+let score = "3abc"
+let play = "3"
+let vals = null
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof vals)
+
+let play1 = Number(play)
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+// NaN means not a number 
+
+console.log(typeof play1)
+console.log(play1)
+
+
+let booln = "Manish"
+let booleanc = Boolean(booln)
+console.log(typeof booleanc);
+console.log(booleanc);
