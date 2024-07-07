dce65fd4100f118bff96c3c86bb79f4fef7b1c9f
Manish Kumar <132132840+manishkumar857@users.noreply.github.com>
2024-07-07 17:09:51 +0000

    comparison of datatypes

---
+++ jsbasic/comparison.js
@@ -0,0 +1,11 @@
+//comparing two diff datatypes
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+
+console.log("2"===2 ); // also checks datatypes
