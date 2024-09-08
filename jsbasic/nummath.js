380673317242a625a2578bfec12abc1197220634
Manish Kumar <132132840+manishkumar857@users.noreply.github.com>
2024-09-08 08:36:17 +0000

    numbers in js

---
+++ jsbasic/nummath.js
@@ -0,0 +1,24 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+console.log(balance.toString());
+console.log(typeof(balance));
+console.log(typeof(score));
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2)); // defining the decimals after number 
+
+const othernumber  = 123.445654
+console.log(othernumber.toPrecision(4)); //defining decimals in number
+
+const hunderds =10000000
+console.log(hunderds.toLocaleString('en-IN')); // PUTING The commas in between digit according to Indian style
+
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+
