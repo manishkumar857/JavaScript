22a981cb140a956756947fdbbfa12796ba59589d
Manish Kumar <132132840+manishkumar857@users.noreply.github.com>
2024-09-08 15:49:10 +0000

    math function

--- jsbasic/nummath.js
+++ jsbasic/nummath.js
@@ -21,4 +21,25 @@ console.log(hunderds.toLocaleString('en-IN')); // PUTING The commas in between d
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
+console.log(Math);
+console.log(Math.abs(-1.4)) //returns the psotive value
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,5,3,1))
+console.log(Math.max(4,6,10,9));
+
+
+console.log(Math.random()); // gives random values between 0 and 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
 
