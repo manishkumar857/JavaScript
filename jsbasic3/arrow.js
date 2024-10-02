6b7f0e50a41a89ecd5258deb5057b86bae8059b6
Manish <jangramanish2602@gmail.com>
2024-10-02 23:11:44 +0530

    arrow in js

--- jsbasic3/arrow.js
+++ jsbasic3/arrow.js
@@ -27,3 +27,24 @@ user.welcomemsg()
 // chai2() // undefined in both type of functions
 
 
+// const chai3 = ()=>{
+//     let username="manish"
+//     console.log(this.username)
+// }
+// chai3() // still undefined fot this.
+
+// const addtwo =(num1,num2)=>{
+//     return num1+num2
+// }
+
+
+// const addtwo =(num1,num2)=>num1+num2
+// console.log(addtwo(3,4));
+
+// const addtwo = (num1,num2)=>(num1+num2)
+// console.log(addtwo(8,9))
+
+const addtwo =(num1,num2)=>({name:"manish"})
+console.log(addtwo(3,5))
+
+/
