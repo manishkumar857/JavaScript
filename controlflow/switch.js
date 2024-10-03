d7831e9966a8ce6b02e74baf623bd2a9f803dfac
Manish <jangramanish2602@gmail.com>
2024-10-03 10:43:44 +0530

    switch case

---
+++ controlflow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const age = 22
+
+switch(age){
+    case 2:
+        console.log("2")
+        break;
+    case 21:
+        console.log("21")
+        break;
+    case 23:
+        console.log("23")
+        break;
+    case 22:
+        console.log("22")
+        break;
+    
+    default:
+        console.log("default case")
+        break;
+}
