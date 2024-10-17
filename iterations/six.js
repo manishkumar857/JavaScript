9a19600765d7bf23470dea6f65c2d8cc3828c477
Manish <jangramanish2602@gmail.com>
2024-10-17 21:47:42 +0530

    other way of filter

--- iterations/six.js
+++ iterations/six.js
@@ -1,14 +1,23 @@
-const coding = ['a','b','c','d','e']
-const values = coding.forEach((item)=>{
-    console.log(item);
-    return item; 
+// const coding = ['a','b','c','d','e']
+// const values = coding.forEach((item)=>{
+//     console.log(item);
+//     return item; 
+// })
+
+// console.log(values)
+
+// const mynum =[2,4,5,6,7,2,9]
+// const newnum = mynum.filter((num)=>{
+//     return num>4
+// })
+
+// console.log(newnum);
+
+const mynums = [1,2,3,4,5,6,7,8]
+const newnum=[]
+mynums.forEach((num)=>{
+    if(num>4){
+        newnum.push(num)
+    }
 })
-
-console.log(values)
-
-const mynum =[2,4,5,6,7,2,9]
-const newnum = mynum.filter((num)=>{
-    return num>4
-})
-
-console.log(newnum);
+console.log(newnum) //other way of filter
