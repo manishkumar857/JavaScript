bb0673f76dddc1dca359bde716fabec0329016f0
Manish <jangramanish2602@gmail.com>
2024-10-17 21:53:56 +0530

    filter in loop ways

--- iterations/six.js
+++ iterations/six.js
@@ -21,3 +21,23 @@ mynums.forEach((num)=>{
     }
 })
 console.log(newnum) //other way of filter
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+let userbooks = books.filter((bk)=> bk.genre==='Science')
+console.log(userbooks);
+
+userbooks=books.filter((bk)=>{
+    return bk.publish===1992 && bk.edition===2008
+})
+console.log(userbooks)
