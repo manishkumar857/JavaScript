7f2c7f6cd37719a91d011194fbc3944dc2f2ee2d
Manish <jangramanish2602@gmail.com>
2025-06-18 19:38:36 +0530

    change

--- Dom.js
+++ Dom.js
@@ -3,60 +3,79 @@
 //console.log -> print html version
 //console.dir -> print object in console
 
-console.dir(document);
-console.dir(document.body);
-console.log(document.body);
-document.body.style.background="pink";
+// console.dir(document);
+// console.dir(document.body);
+// console.log(document.body);
+// document.body.style.background="pink";
 
-console.dir(document.body.childNodes[1]);
+// console.dir(document.body.childNodes[1]);
 
-document.body.childNodes[1].innerText = "Heading Changed";
+// document.body.childNodes[1].innerText = "Heading Changed";
 
-let heading = document.getElementById("heading");
-console.dir(heading);
+// let heading = document.getElementById("heading");
+// console.dir(heading);
 
-let class1 = document.getElementsByClassName("back");
-console.dir(class1);
+// let class1 = document.getElementsByClassName("back");
+// console.dir(class1);
 
-let para = document.getElementsByTagName("p");
-console.dir(para);
+// let para = document.getElementsByTagName("p");
+// console.dir(para);
 
-//query selector
+// //query selector
 
-// let firstelement = document.querySelector("p");//return matching first element
+// // let firstelement = document.querySelector("p");//return matching first element
+// // console.dir(firstelement);
+
+// // let allelement = document.querySelectorAll("p"); //return all matching element
+// // console.dir(allelement);
+
+// // let firstelement = document.querySelector(".back");//return matching first element
+// // console.dir(firstelement);
+
+// // let allelement = document.querySelectorAll(".back"); //return all matching element
+// // console.dir(allelement);
+
+// let firstelement = document.querySelector("#heading");//return matching first element
 // console.dir(firstelement);
 
-// let allelement = document.querySelectorAll("p"); //return all matching element
-// console.dir(allelement);
 
-// let firstelement = document.querySelector(".back");//return matching first element
-// console.dir(firstelement);
+// //dom manipulation
 
-// let allelement = document.querySelectorAll(".back"); //return all matching element
-// console.dir(allelement);
+// console.dir(firstelement.tagName);
 
-let firstelement = document.querySelector("#heading");//return matching first element
-console.dir(firstelement);
+// console.dir(document.querySelector("body").children);
+// console.dir(document.querySelector("body").firstChild);
+// console.dir(document.querySelector("body").lastChild);
 
 
-//dom manipulation
-
-console.dir(firstelement.tagName);
-
-console.dir(document.querySelector("body").children);
-console.dir(document.querySelector("body").firstChild);
-console.dir(document.querySelector("body").lastChild);
+// let bd = document.querySelector("body");
+// console.dir(bd.innerText); //pure text
+// console.dir(bd.innerHTML); //text with html content 
 
 
-let bd = document.querySelector("body");
-console.dir(bd.innerText); //pure text
-console.dir(bd.innerHTML); //text with html content 
+// let h1 = document.querySelector("h1");
+// h1.innerHTML="<i>heading changed again</i>"
+
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// console.dir(h2.textContent); //display hidden content as well
 
 
-let h1 = document.querySelector("h1");
-h1.innerHTML="<i>heading changed again</i>"
+//attributes
+// console.dir(h2.getAttribute("style"));
+// console.dir(h2.setAttribute("style","visibility:visible"));
 
-let h2=document.querySelector("h2");
-console.dir(h2.innerText);
+// console.dir(h2.style);
+// h2.style.backgroundColor = "yellow";
 
-console.dir(h2.textContent); //display hidden content as well
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click me !";
+
+let body = document.querySelector("body");
+
+//body.append(newBtn);
+body.prepend(newBtn);
+
+//node .remove like body.remove;
